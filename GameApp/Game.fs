adc2a8e507726c9d51501960d3d9b59f25ee70c7
Damien LeBerrigaud <git@itsdamo.com>
2021-06-10 08:12:57 -0600

    Improve scaling and initial graphics setup.

--- GameApp/Game.fs
+++ GameApp/Game.fs
@@ -4,38 +4,42 @@ open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
 open Microsoft.Xna.Framework.Input
 
-let pixelWidth = 640
-let pixelHeight = 360
-let pixelAspect = float pixelWidth / float pixelHeight
+let canvas = Rectangle (0, 0, 640, 360)
+let multiSampleCount = 1
 
-type GameLoop () as x =
+let flooredInt = floor >> int
+let calculateScale a b = min a b |> ((*) 2.0) |> floor |> (*) 0.5
+
+type GameLoop () as this =
     inherit Game()
 
-    let graphics = new GraphicsDeviceManager(x)
-
-    do x.Content.RootDirectory <- "Content"
-    do x.Window.AllowUserResizing <- true
+    let graphics = new GraphicsDeviceManager(this)
 
     let mutable renderTarget = Unchecked.defaultof<RenderTarget2D>
     let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
     let mutable font = Unchecked.defaultof<SpriteFont>
-
     let mutable recordedKeyboardState: KeyboardState option = None
 
-    override x.Initialize() =
+    override this.Initialize() =
+        this.Content.RootDirectory <- "Content"
+        this.Window.AllowUserResizing <- true
+
+        graphics.PreferMultiSampling <- false
+        graphics.SynchronizeWithVerticalRetrace <- true
+        graphics.HardwareModeSwitch <- false
+        graphics.PreferredBackBufferWidth <- canvas.Width * 2
+        graphics.PreferredBackBufferHeight <- canvas.Height * 2
+        graphics.ApplyChanges()
+
         base.Initialize()
-        ()
 
     override this.LoadContent() =
-        let pp = graphics.GraphicsDevice.PresentationParameters
-
-        renderTarget <- new RenderTarget2D(x.GraphicsDevice, pixelWidth, pixelHeight, false,
-            SurfaceFormat.Color, DepthFormat.None, pp.MultiSampleCount, RenderTargetUsage.DiscardContents)
-        spriteBatch <- new SpriteBatch(x.GraphicsDevice)
-        font <- x.Content.Load<SpriteFont>("Fonts/PressStart_Regular_17")
+        renderTarget <- new RenderTarget2D(this.GraphicsDevice, canvas.Width, canvas.Height)
+        spriteBatch <- new SpriteBatch(this.GraphicsDevice)
+        font <- this.Content.Load<SpriteFont>("Fonts/PressStart_Regular_17")
         ()
 
-    override this.Update (gameTime) =
+    override this.Update _ =
         let currentState = Keyboard.GetState()
         let previousState = recordedKeyboardState |> Option.defaultValue currentState
 
@@ -47,35 +51,31 @@ type GameLoop () as x =
         recordedKeyboardState <- Some currentState
         ()
 
-    override this.Draw (gameTime) =
-        x.GraphicsDevice.SetRenderTarget(renderTarget)
-        x.GraphicsDevice.Clear Color.CornflowerBlue
+    override this.Draw _ =
+        this.GraphicsDevice.SetRenderTarget(renderTarget)
+        this.GraphicsDevice.Clear Color.CornflowerBlue
 
-        spriteBatch.Begin()
+        spriteBatch.Begin(samplerState = SamplerState.PointClamp)
         spriteBatch.DrawString(font, "Hello", Vector2.Zero, Color.WhiteSmoke)
         spriteBatch.End()
 
-        let windowBounds = x.Window.ClientBounds
-        let windowWidth = float windowBounds.Width
-        let windowHeight = float windowBounds.Height
+        let viewBounds = this.Window.ClientBounds
+        let viewWidth = float viewBounds.Width
+        let viewHeight = float viewBounds.Height
 
-        let roundDown = floor >> int
-        let multiplier = roundDown (windowHeight / float pixelHeight)
-        let targetWidth = pixelWidth * multiplier
-        let targetHeight = pixelHeight * multiplier
+        let scale = calculateScale (viewWidth / (float canvas.Width)) (viewHeight / (float canvas.Height))
 
-        let destination =
-            Rectangle(
-                roundDown ((windowWidth - float targetWidth) / 2.0),
-                roundDown ((windowHeight - float targetHeight) / 2.0),
-                targetWidth,
-                targetHeight
-            )
+        let targetWidth = (float canvas.Width) * scale |> flooredInt
+        let targetHeight = (float canvas.Height) * scale |> flooredInt
+        let x = (viewWidth - float targetWidth) / 2.0 |> flooredInt
+        let y = (viewHeight - float targetHeight) / 2.0 |> flooredInt
 
-        x.GraphicsDevice.SetRenderTarget(null)
+        let destination = Rectangle (x, y, targetWidth, targetHeight)
 
-        x.GraphicsDevice.Clear Color.Black
-        spriteBatch.Begin()
+        this.GraphicsDevice.SetRenderTarget(null)
+        this.GraphicsDevice.Clear Color.Black
+
+        spriteBatch.Begin(samplerState = SamplerState.PointClamp)
         spriteBatch.Draw(renderTarget, destination, Color.White)
         spriteBatch.End()
         ()
