0e8c1b8ea7af553d149c2d003aad230647ec7578
Damien LeBerrigaud <git@itsdamo.com>
2021-06-15 17:54:27 -0600

    Better description of the function.

--- GameApp/Prelude/GameTime.fs
+++ GameApp/Prelude/GameTime.fs
@@ -4,7 +4,15 @@ open Microsoft.Xna.Framework
 
 [<RequireQualifiedAccess>]
 module VelocityPerSecond =
-    // Takes velocity per second `v`
-    // returns the velocity for the given elapsed time.
+
+    /// <summary>
+    /// Returns the velocity for a given elapsed time.
+    /// </summary>
+    /// <param name="v">
+    /// The velocity per second
+    /// </param>
+    /// <param name="t">
+    /// The GameTime to apply to the velocity per second
+    /// </param>
     let forGameTime (v: float32) (t: GameTime) =
         v * (float32 t.ElapsedGameTime.Milliseconds / 1_000.0f)
